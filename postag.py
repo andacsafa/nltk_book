76ff29f2f66b35c62229d68a6ec851a29e9a474e
andacsafa <andac.safa@gmail.com>
2015-08-31 15:03:27 +0300

    postag added

---
+++ postag.py
@@ -0,0 +1,32 @@
+import nltk
+from nltk.corpus import brown
+
+def findtags(tag_prefix, tagged_text):
+    cfd = nltk.ConditionalFreqDist((tag, word) for (word, tag) in tagged_text
+            if tag.startswith(tag_prefix))
+    return dict((tag, cfd[tag].most_common(5)) for tag in cfd.conditions())
+
+
+def process(sentence):
+    for (w1, t1), (w2, t2), (w3, t3) in nltk.trigrams(sentence):
+        if (t1.startswith('V') and t2 == 'TO' and t3.startswith('V')):
+            print(w1, w2, w3)
+
+def performance(cfd, wordlist):
+    lt = dict((word, cfd[word].max()) for word in wordlist)
+    baseline_tagger = nltk.UnigramTagger(model=lt, backoff=nltk.DefaultTagger('NN'))
+    return baseline_tagger.evaluate(brown.tagged_sents(categories='news'))
+
+
+def display():
+    import pylab
+    word_freqs = nltk.FreqDist(brown.words(categories='news')).most_common()
+    words_by_freq = [w for (w, _) in word_freqs]
+    cfd = nlkt.ConditionalFreqDist(brown.tagged_words(categories='news'))
+    sizes = 2 ** pylab.arange(15)
+    perfs = [performance(cfd, words_by_freq[:size]) for size in sizes]
+    pylab.plot(sizes, perfs, '-bo')
+    pylab.title('Lookup Tagger Performance with Varying Model Size')
+    pylab.xlabel('Model Size')
+    pylab.ylabel('Performance')
+    pylab.show()
